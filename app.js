7ff1f005d48d00ed3840fadacb9e2f295c236a50
david <dheis24+git@gmail.com>
2020-01-06 11:47:41 +1000

    remove inline style

--- app.js
+++ app.js
@@ -8,12 +8,10 @@ const webhoseio = require('webhoseio');
 const contactsController = require('./controllers/contact')
 const webhoseioClient = webhoseio.config({token: '592afc32-9f3a-4a8b-9ed2-027d06228fae'});
 
-
 const fs = require('fs');
 
 let rawCapodata = fs.readFileSync('database/shop.json');
 let allCapos = JSON.parse(rawCapodata);
-
 
 // // Get a reference to the database service
 // const firebase = require('firebase');
@@ -30,21 +28,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-
 app.post('/contact', contactsController.sendContactPageEmail);
-
 app.get('/contact', (req, res) => {
     res.render('contact');
 });
-
 app.get('/capos', (req, res) => {
     res.render('capos', {caposArray: Object.values(allCapos)});
 });
-
 app.get('/returns', (req, res) => {
     res.render('returns');
 });
-
 app.get('/what-is-flamenco', (req, res) => {
     res.render('what-is-flamenco');
 });
@@ -52,23 +45,17 @@ app.get('/spanish-guitar', (req, res) => {
     title = "The Spanish Flamenco Guitar"
     res.render('spanish-guitar',{title: title});
 });
-
 app.get('/*', (req, res) => {
     const query_params = {
-        "site_type": "news",
-        "language": "spanish",
+        "q": "thread.url:https* spam_score:<0.6 site_type:news language:spanish thread.country:ES",
         "sort": "crawled",
         "size": "6",
-        "spam_score": "0",
-        "format": "json",
-        "thread.country":"ES"
-        // "site_category": "music"
+        "format": "json"
         }
         webhoseioClient.query('filterWebContent', query_params)
     .then(newsBody => {
         res.render('index', {newsBody: newsBody.posts})
     });
-  
     });
 // Object.values() this gets the value of objects and puts them in an array 
 // Object.keys() gets keys and puts the in an array
