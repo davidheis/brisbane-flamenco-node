5dc657a5666a2d8a9c33db466bef4cbd5eb3e2e2
david <dheis24+git@gmail.com>
2020-01-26 20:26:33 -0500

    add csp post route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const contactsController = require('./controllers/contact')
 const webhoseioClient = webhoseio.config({token: '592afc32-9f3a-4a8b-9ed2-027d06228fae'});
 // require('bootstrap')
 const fs = require('fs');
+const helmet = require('helmet')
 let rawCapodata = fs.readFileSync('database/shop.json');
 let allCapos = JSON.parse(rawCapodata);
 
@@ -20,12 +21,10 @@ let allCapos = JSON.parse(rawCapodata);
 // // end firebase
 
 const app = express();
+app.use(helmet())
 app.use(bodyParser.json({
-    type: ['json', 'application/csp-report']
+    type: ['json', 'application/csp-report','application/json']
   }))
-  app.use(bodyParser.json({type: 'application/csp-report'}));
-  app.use(bodyParser.json({type: 'application/json'}));
-app.use(bodyParser.json({type: 'application/csp-report'}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
