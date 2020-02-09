f8e5ea13f57d9709031c23aa12648cf865b1b329
david <dheis24+git@gmail.com>
2020-02-09 05:08:12 -0500

    remove negative margin top on mobile nav

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -1,16 +1,20 @@
 window.onload = function preprareNavDropdown() {
     var isNavOpen = false;
     var mobileNavBtn = document.getElementById("mobileNavBtn");
+    var mobileNav = document.getElementById("mobileNav");
+
     mobileNavBtn.onclick = function () {
         // use to toggle onclick on and off
         isNavOpen = !isNavOpen;
         if (isNavOpen) {
             // show nav
-            document.getElementById("mobileNav").style = "display:flex;opacity:100%;"
+            mobileNav.style = "display:flex;margin-top: 0;"
+            mobileNavBtn.style = "margin-bottom: 0;"
         } 
         else {
             // hide nav
-            document.getElementById("mobileNav").style = "display:none;opacity:0;"
+            mobileNav.style = "display:none;"
+            mobileNavBtn.style = "margin-bottom: 100px;"
         }
     }
 }
